4596a023e798dec8b469f40d4107ec6b30e38af3
Ksenia <stopisem@inbox.ru>
2023-02-23 13:25:14 +0500

    Task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a == b * b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
