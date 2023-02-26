95ca295d97e90e450910dcfb6f95ffefd2a811b3
Ksenia <stopisem@inbox.ru>
2023-02-26 23:16:06 +0500

    HW Task1

---
+++ Lesson_1/HomeWork/Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else if(a < b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else 
+{
+    Console.WriteLine($"max = {a}");
+}
