c6f92849c805f8394113fb1f306ba750d895bb0d
Ksenia <stopisem@inbox.ru>
2023-02-27 00:07:19 +0500

    HW Task4

---
+++ Lesson_1/HomeWork/Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+ Console.WriteLine();
+
+int count = 2;
+while(count < a)
+{
+    Console.WriteLine($"{count} ");
+    count = count + 2;
+}
+
