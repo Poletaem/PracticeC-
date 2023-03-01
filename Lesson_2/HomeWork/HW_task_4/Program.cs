2050586e94b281c0c272d906274b29a96bfacb14
Ksenia <stopisem@inbox.ru>
2023-03-01 21:16:57 +0500

    Home_4

---
+++ Lesson_2/HomeWork/HW_task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+int num = int.Parse(Console.ReadLine()!);
+
+void Week(int num)
+
+{
+    if (num > 0 & num <6)
+        Console.WriteLine("Будний день");
+    else if(num == 6 || num == 7)
+        Console.WriteLine("Выходной");
+    else
+        Console.WriteLine("Error");
+
+    
+}   
+
+Week(num);
