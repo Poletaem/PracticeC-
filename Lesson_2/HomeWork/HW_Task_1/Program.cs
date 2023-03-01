a3b3078ca1a74cac553be12e97fbcbb4d3f5fb1f
Ksenia <stopisem@inbox.ru>
2023-03-01 13:38:10 +0500

    Home_1

---
+++ Lesson_2/HomeWork/HW_Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int num = int.Parse(Console.ReadLine()!);
+
+int SecNum(int num)
+{
+    if(num > -1000 & num < -99 || num > 99 & num <1000)
+        return num % 100 /10;
+    else 
+        return num;
+}
+
+int result = SecNum(num);
+Console.WriteLine(result);
