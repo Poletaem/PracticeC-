ac62a5b7cd11a063ba640ccb8803e87f016822bc
Ksenia <stopisem@inbox.ru>
2023-03-08 07:56:15 +0500

    HW_1

--- Lesson_4/4_3/Program.cs
+++ Lesson_4/4_3/Program.cs
@@ -1,7 +1,8 @@
 ﻿// 3. Напишите программу, которая выводит массив из 8 элементов,
 //    заполненный нулями и единицами в случайном порядке.
 
-void massive(int mas){
+void massive(int mas)
+{
     int[] numbers =  new int[mas];
     for (int i = 0; i < mas; i++)
     {
---
+++ Lesson_4/HomeWork/HW_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int Prod(int A, int B)
+
+{
+    int pov = 1;
+
+    for(int i = 1; i <= B; i++)
+    pov = pov * A;
+
+    return pov;
+}
+
+Console.WriteLine("Введите числа: ");
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+
+int result =  Prod(A, B);
+Console.WriteLine(result);
