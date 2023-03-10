274497b6f44377e2473798f81573de80cb50acae
Ksenia <stopisem@inbox.ru>
2023-03-10 14:15:52 +0500

    HomeWork 5_2

---
+++ Lesson_5/HomeWork/HW_2/Program.cs
@@ -0,0 +1,44 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+
+int SumMass(int[] Mass)
+{
+    int Sum = 0;
+    for (int i = 0; i < Mass.Length; i = i + 2)
+        Sum = Sum + Mass[i];
+    return Sum;
+}
+
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Диапазон от");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Диапазон до");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+int result = SumMass(mass);
+Console.WriteLine(result);
