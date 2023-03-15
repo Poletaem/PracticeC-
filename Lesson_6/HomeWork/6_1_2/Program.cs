50839d54ab5dc83fe4256bffb8ef656246182ee5
Ksenia <stopisem@inbox.ru>
2023-03-15 13:16:38 +0500

    HW_1

---
+++ Lesson_6/HomeWork/6_1_2/Program.cs
@@ -0,0 +1,41 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+
+
+    Console.Write("Введите количество чисел:");
+    int count = int.Parse(Console.ReadLine()!);
+    int[] Array = new int[count];
+
+void  FillMass(int count)
+{
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"Введите числа:    ");
+        Array[i] = int.Parse(Console.ReadLine()!);
+      
+    }
+              
+}       
+
+FillMass(count);
+Console.WriteLine();
+
+int AmtMass(int[] Array)
+
+{
+    int amt = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+            amt += 1;
+    }
+    return amt;
+}
+
+int result = AmtMass(Array);
+Console.WriteLine($"Количество чисел больше 0 = {result}" );
+
