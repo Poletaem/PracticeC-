200fa2739e7108c4da639bbeca677f3c1893112d
Ksenia <stopisem@inbox.ru>
2023-03-19 22:58:16 +0500

    HW_7_1

---
+++ Lesson_7/HomeWork/HW_7_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+
+
+void Print(double[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+double[,] FillMass(int row, int column, double from, double to)
+{
+    double[,] arr = new double[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] =  Math.Round(new Random().NextDouble() * (from - to) + to, 2);
+    return arr;
+}
+
+
+Console.WriteLine("Введите количество строк:    ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов:    ");
+int column_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите начало диапазона:    ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конец диапазона:    ");
+int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+double[,] mass = FillMass(row_num, column_num, start, stop);
+Print(mass);
